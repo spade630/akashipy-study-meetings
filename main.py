68b4bf708ff039b7e6e4255ce60eafc41f87b700
spade630 <spade6301@gmail.com>
2015-07-05 16:52:55 +0900

    Add runtime.txt

--- main.py
+++ main.py
@@ -21,4 +21,8 @@ def events(keyword):
     r = requests.get('http://connpass.com/api/v1/event/?keyword='+keyword)
     return template('events', events=r.json()['events'][:10])
 
-run(host='localhost', port=8080)
+from os import environ
+if environ.get('IS_HEROKU'):
+    run(server='gunicorn', host='0.0.0.0', port=int(environ.get("PORT",5000)))
+else:
+    run(host='localhost', port=8080, debug=True, reloader=True)
