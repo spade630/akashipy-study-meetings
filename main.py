eb15749288096c0fd20e63f17e45607d04e3a56e
spade630 <spade6301@gmail.com>
2015-07-05 15:26:46 +0900

    Python勉強会一覧表示追加

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from bottle import route, run, template
+import requests
 
 @route('/')
 def index():
-    return '<b>Hello World!</b>\n<a href=\"./hello\">hello?</a>'
+    return '<b>Hello World!</b>\n<a href=\"./events\">Events?</a>'
 
 @route('/hello')
 def index():
     return template('hello', name='ゆーり')
 
 @route('/events')
-def index():
-    return template('events')
+def events():
+    r = requests.get('http://connpass.com/api/v1/event/?keyword=python')
+    return template('events', events=r.json()['events'][:10])
 
 run(host='localhost', port=8080, debug=True, reloader=True)
--- views/events.tpl
+++ views/events.tpl
@@ -1,6 +1,5 @@
 % rebase('base.tpl', title='勉強会の一覧')
     <h1>勉強会の一覧</h1>
-    <p>event1</p>
-    <p>event2</p>
-    <p>event3</p>
-    <p>event4</p>
+    % for event in events:
+        <p><a href="{{event['event_url']}}">{{event['title']}}</a></p>
+    %
